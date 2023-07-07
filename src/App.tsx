e37c3346f6cecb922e0284b297bbbf7d14258ddb
Pavel <ubuntu@ubuntu.ru>
2023-07-07 16:53:42 +0500

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="header">
+        <div className="container"> 
+          <div className="header__row">
+
+            <div className='header__block'>
+              <div className='header__rectangle'></div>
+              <div className='header__rectangle'></div>
+              <div className='header__rectangle'></div>
+              <div className='header__rectangle'></div>
+              <div className='header__rectangle'></div>
+            </div> 
+             
+          </div>
+        </div>
+      </div>
+      <div className='main'>
+        <div className='container'>
+          <div className='main__three'>
+            <div className='main__item'>
+                <div className='main__rectangle'>
+                  <svg xmlns="http://www.w3.org/2000/svg">
+                    <rect width="100%" height="100%" fill="#eee49a"></rect>
+                  </svg>
+                </div>
+                <div className='main__text'>
+                  Sed ut perspiciatis unde omnis iste natus error sit 
+                  voluptatem accusantium doloremque laudantium, totam 
+                  rem aperiam, eaque ipsa quae ab illo inventore veritatis
+                  et quasi architecto beatae vitae dicta sunt explicabo. 
+                </div>
+            </div>
+            <div className='main__item second'>
+                <div className='main__rectangle'>
+                  <svg xmlns="http://www.w3.org/2000/svg">
+                    <rect width="100%" height="100%" fill="#eee49a"></rect>
+                  </svg>
+                </div>
+                <div className='main__text'>
+                  Sed ut perspiciatis unde omnis iste natus error sit 
+                  voluptatem accusantium doloremque laudantium, totam 
+                  rem aperiam, eaque ipsa quae ab illo inventore veritatis
+                  et quasi architecto beatae vitae dicta sunt explicabo. 
+                </div>
+            </div>
+            <div className='main__item'>
+              <div className='main__rectangle'>
+                <svg xmlns="http://www.w3.org/2000/svg">
+                  <rect width="100%" height="100%" fill="#eee49a"></rect>
+                </svg>
+              </div>
+              <div className='main__text'>
+                  Sed ut perspiciatis unde omnis iste natus error sit 
+                  voluptatem accusantium doloremque laudantium, totam 
+                  rem aperiam, eaque ipsa quae ab illo inventore veritatis
+                  et quasi architecto beatae vitae dicta sunt explicabo. 
+                </div>
+            </div>
+          </div>
+          <div className='main__second'>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
