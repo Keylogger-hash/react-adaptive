0d04d3b41c83b4c22f6db67d5a2fd389813ce10a
Павел Морозов <pavelmorozov@MacBook-Air-Pavel.local>
2023-07-09 22:03:59 +0500

    Finish make maket

--- src/App.tsx
+++ src/App.tsx
@@ -3,67 +3,93 @@ import './App.css';
 
 function App() {
   return (
-    <div className="wrapper">
-      <div className="header">
-        <div className="container"> 
-          <div className="header__row">
-
-            <div className='header__block'>
-              <div className='header__rectangle'></div>
-              <div className='header__rectangle'></div>
-              <div className='header__rectangle'></div>
-              <div className='header__rectangle'></div>
-              <div className='header__rectangle'></div>
-            </div> 
-             
-          </div>
+    <div className='wrapper'>
+      <div className='header'>
+        <div className='container'>
+            <div className='header_wrapper'>
+              <div className='header_logo_block'>
+                <div className='header_logo'>
+                </div>
+              </div>
+              <div className='header_block'>
+                <div className='header_item'>
+                  
+                </div>
+                <div className='header_item'>
+                  
+                </div>
+                <div className='header_item'>
+                  
+                </div>
+                <div className='header_item'>
+                  
+                </div>
+                <div className='header_item'>
+                  
+                </div>
+              </div>
+              <div className='header_burger'>
+                <div className='header_burger_button'>
+                  
+                </div>
+              </div>
+            </div>
         </div>
       </div>
       <div className='main'>
         <div className='container'>
-          <div className='main__three'>
-            <div className='main__item'>
-                <div className='main__rectangle'>
-                  <svg xmlns="http://www.w3.org/2000/svg">
-                    <rect width="100%" height="100%" fill="#eee49a"></rect>
-                  </svg>
+          <div className='main_wrapper'>
+            <div className='three_item'>
+              <div className='main_item'>
+                <div className='main_image'>
                 </div>
-                <div className='main__text'>
-                  Sed ut perspiciatis unde omnis iste natus error sit 
-                  voluptatem accusantium doloremque laudantium, totam 
-                  rem aperiam, eaque ipsa quae ab illo inventore veritatis
-                  et quasi architecto beatae vitae dicta sunt explicabo. 
+                <div className='main_text'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque justo nisi, non pretium tortor egestas nec. Proin ultricies augue eu enim accumsan hendrerit non et mi. Morbi tempor elit libero. Ut euismod augue elit, id efficitur tortor bibendum et. Quisque sed gravida sem. Aliquam nec dignissim ante. Vivamus ac placerat magna, vitae feugiat nulla. Duis et elit nec mauris porta efficitur.
                 </div>
-            </div>
-            <div className='main__item second'>
-                <div className='main__rectangle'>
-                  <svg xmlns="http://www.w3.org/2000/svg">
-                    <rect width="100%" height="100%" fill="#eee49a"></rect>
-                  </svg>
-                </div>
-                <div className='main__text'>
-                  Sed ut perspiciatis unde omnis iste natus error sit 
-                  voluptatem accusantium doloremque laudantium, totam 
-                  rem aperiam, eaque ipsa quae ab illo inventore veritatis
-                  et quasi architecto beatae vitae dicta sunt explicabo. 
-                </div>
-            </div>
-            <div className='main__item'>
-              <div className='main__rectangle'>
-                <svg xmlns="http://www.w3.org/2000/svg">
-                  <rect width="100%" height="100%" fill="#eee49a"></rect>
-                </svg>
               </div>
-              <div className='main__text'>
-                  Sed ut perspiciatis unde omnis iste natus error sit 
-                  voluptatem accusantium doloremque laudantium, totam 
-                  rem aperiam, eaque ipsa quae ab illo inventore veritatis
-                  et quasi architecto beatae vitae dicta sunt explicabo. 
+              <div className='main_item'>
+                <div className='main_image'>
                 </div>
+                <div className='main_text'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque justo nisi, non pretium tortor egestas nec. Proin ultricies augue eu enim accumsan hendrerit non et mi. Morbi tempor elit libero. Ut euismod augue elit, id efficitur tortor bibendum et. Quisque sed gravida sem. Aliquam nec dignissim ante. Vivamus ac placerat magna, vitae feugiat nulla. Duis et elit nec mauris porta efficitur.
+                </div>
+              </div>
+              <div className='main_item'>
+                <div className='main_image'>
+                </div>
+                <div className='main_text'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque justo nisi, non pretium tortor egestas nec. Proin ultricies augue eu enim accumsan hendrerit non et mi. Morbi tempor elit libero. Ut euismod augue elit, id efficitur tortor bibendum et. Quisque sed gravida sem. Aliquam nec dignissim ante. Vivamus ac placerat magna, vitae feugiat nulla. Duis et elit nec mauris porta efficitur.
+                </div>
+              </div>
             </div>
-          </div>
-          <div className='main__second'>
-
+            <div className='second_item'>
+              <div className='main_second_item'>
+                <div className='main_image'>
+                </div>
+                <div className='main_text'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque justo nisi, non pretium tortor egestas nec. Proin ultricies augue eu enim accumsan hendrerit non et mi. Morbi tempor elit libero. Ut euismod augue elit, id efficitur tortor bibendum et. Quisque sed gravida sem. Aliquam nec dignissim ante. Vivamus ac placerat magna, vitae feugiat nulla. Duis et elit nec mauris porta efficitur.
+                </div>
+              </div>
+              <div className='main_second_item'>
+                <div className='main_image'>
+                </div>
+                <div className='main_text'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque justo nisi, non pretium tortor egestas nec. Proin ultricies augue eu enim accumsan hendrerit non et mi. Morbi tempor elit libero. Ut euismod augue elit, id efficitur tortor bibendum et. Quisque sed gravida sem. Aliquam nec dignissim ante. Vivamus ac placerat magna, vitae feugiat nulla. Duis et elit nec mauris porta efficitur.
+                </div>
+              </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+      <div className='footer'>
+        <div className='container'>
+          <div className='footer_wrapper'>
+            <div className='footer_item'>
+            </div>
+            <div className='footer_item'>
+            </div>
+            <div className='footer_item'>
+            </div>
           </div>
         </div>
       </div>
